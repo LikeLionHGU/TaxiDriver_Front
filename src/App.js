d8f959cd077d63754e4b170769aeeb051c23dc24
Haeun <sunghaeun1004@gmail.com>
2025-08-13 05:28:50 +0900

    feat : 경로 설정 세팅 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<h1>Welcome to the Taxi Driver App</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
