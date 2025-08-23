38857559a4067952b944746b0cd229289448e73b
JinHeeWond <jin2749708@handong.ac.kr>
2025-08-23 21:28:33 +0900

    style:구매업체,위판장,어부 회원가입 페이지 스타일

--- src/pages/FishmanForm.js
+++ src/pages/FishmanForm.js
@@ -6,155 +6,179 @@ import styles from "../styles/consignment-company-form.module.css"
 import { ReactComponent as LocationIcon } from '../assets/위치.svg';
 
 export default function LocationSelector() {
-  const [selectedProvince, setSelectedProvince] = useState("경상도")
-  const [selectedLocation, setSelectedLocation] = useState("포항 구룡포")
+  const [selectedProvince, setSelectedProvince] = useState("") // 빈 문자열로 초기화
+  const [selectedLocation, setSelectedLocation] = useState("") // 빈 문자열로 초기화
 
   const provinces = ["강원도", "경상도", "경기/인천", "전라도", "제주도", "충청도"]
 
-  const locations = [
-    { name: "포항 죽도시장", city: "포항" },
-    { name: "포항 구룡포", city: "포항" },
-    { name: "부산 자갈치시장", city: "부산" },
-    { name: "부산 기장", city: "부산" },
-    { name: "통영 중앙시장", city: "통영" },
-    { name: "거제 외포항", city: "거제" },
-    { name: "울산 울진", city: "울산" },
-  ]
+  // 지역별 위판장 데이터
+  const locationsByProvince = {
+    "강원도": [
+      { name: "강릉 중앙시장", city: "강릉" },
+      { name: "속초 동명항", city: "속초" },
+    ],
+    "경상도": [
+      { name: "포항 죽도시장", city: "포항" },
+      { name: "포항 구룡포", city: "포항" },
+      { name: "부산 자갈치시장", city: "부산" },
+      { name: "부산 기장", city: "부산" },
+      { name: "통영 중앙시장", city: "통영" },
+      { name: "거제 외포항", city: "거제" },
+      { name: "울산 울진", city: "울산" },
+    ],
+    "경기/인천": [
+      { name: "인천 소래포구", city: "인천" },
+      { name: "인천 연안부두", city: "인천" },
+    ],
+    "전라도": [
+      { name: "완도 수산시장", city: "완도" },
+      { name: "여수 돌산대교", city: "여수" },
+    ],
+    "제주도": [
+      { name: "제주 동문시장", city: "제주" },
+      { name: "서귀포 올레시장", city: "서귀포" },
+    ],
+    "충청도": [
+      { name: "보령 대천항", city: "보령" },
+      { name: "태안 안면도", city: "태안" },
+    ]
+  }
+
+  // 선택된 도에 따른 위판장 목록
+  const currentLocations = selectedProvince ? locationsByProvince[selectedProvince] || [] : []
+
+  const handleProvinceSelect = (province) => {
+    setSelectedProvince(province)
+    setSelectedLocation("") // 도가 바뀌면 선택된 위판장 초기화
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.header}>
-        <svg width="111" height="111" viewBox="0 0 111 111" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_2678_1058)">
-<circle cx="55.6016" cy="51.9217" r="49.075" fill="white"/>
-</g>
-<g filter="url(#filter1_f_2678_1058)">
-<path d="M53.9417 50.6729C55.532 51.591 55.532 53.8864 53.9417 54.8045L37.5441 64.2717C35.7479 65.3087 33.5752 63.7175 34.022 61.692L35.5436 54.794C35.8423 53.4401 35.8423 52.0373 35.5436 50.6834L34.022 43.7854C33.5753 41.76 35.7479 40.1687 37.5441 41.2058L53.9417 50.6729Z" fill="url(#paint0_linear_2678_1058)" fill-opacity="0.8"/>
-</g>
-<g filter="url(#filter2_f_2678_1058)">
-<path d="M53.9417 49.587C55.532 50.5051 55.532 52.8004 53.9417 53.7186L37.5441 63.1857C35.7479 64.2228 33.5752 62.6315 34.022 60.6061L35.5436 53.7081C35.8423 52.3542 35.8423 50.9514 35.5436 49.5974L34.022 42.6995C33.5753 40.674 35.7479 39.0827 37.5441 40.1198L53.9417 49.587Z" fill="url(#paint1_linear_2678_1058)" fill-opacity="0.88"/>
-</g>
-<g filter="url(#filter3_f_2678_1058)">
-<path d="M74.769 49.8556C76.3592 50.7737 76.3592 53.0691 74.769 53.9872L45.7885 70.719C43.9923 71.7561 41.8197 70.1648 42.2665 68.1394L45.4661 53.6342C45.715 52.5059 45.715 51.3369 45.4661 50.2086L42.2665 35.7034C41.8197 33.6779 43.9923 32.0867 45.7885 33.1237L74.769 49.8556Z" fill="url(#paint2_linear_2678_1058)" fill-opacity="0.72"/>
-</g>
-<g filter="url(#filter4_f_2678_1058)">
-<path d="M73.5758 49.9096C75.1598 50.8289 75.1598 53.1165 73.5758 54.0358L45.7915 70.1612C43.9965 71.203 41.8194 69.6141 42.2642 67.5868L45.3166 53.677C45.5629 52.5541 45.5629 51.3913 45.3166 50.2684L42.2642 36.3586C41.8194 34.3313 43.9965 32.7424 45.7915 33.7842L73.5758 49.9096Z" fill="white" fill-opacity="0.16"/>
-</g>
-<g filter="url(#filter5_f_2678_1058)">
-<circle cx="63.6478" cy="51.6527" r="2.5287" fill="white" fill-opacity="0.86"/>
-</g>
-<defs>
-<filter id="filter0_d_2678_1058" x="0.900119" y="0.850183" width="109.403" height="109.403" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feMorphology radius="0.907499" operator="erode" in="SourceAlpha" result="effect1_dropShadow_2678_1058"/>
-<feOffset dy="3.63"/>
-<feGaussianBlur stdDeviation="3.267"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.07 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2678_1058"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2678_1058" result="shape"/>
-</filter>
-<filter id="filter1_f_2678_1058" x="28.397" y="35.314" width="32.3033" height="34.8496" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="0.318052" result="effect1_foregroundBlur_2678_1058"/>
-</filter>
-<filter id="filter2_f_2678_1058" x="31.5775" y="37.4086" width="25.9423" height="28.4886" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="0.318052" result="effect1_foregroundBlur_2678_1058"/>
-</filter>
-<filter id="filter3_f_2678_1058" x="41.4121" y="32.0027" width="35.3447" height="39.8373" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="0.397565" result="effect1_foregroundBlur_2678_1058"/>
-</filter>
-<filter id="filter4_f_2678_1058" x="40.6154" y="31.8648" width="35.7386" height="40.2157" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="0.795129" result="effect1_foregroundBlur_2678_1058"/>
-</filter>
-<filter id="filter5_f_2678_1058" x="60.483" y="48.4879" width="6.32958" height="6.32982" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="0.318052" result="effect1_foregroundBlur_2678_1058"/>
-</filter>
-<linearGradient id="paint0_linear_2678_1058" x1="45.1879" y1="38.499" x2="45.1879" y2="66.9784" gradientUnits="userSpaceOnUse">
-<stop stop-color="#1595F6"/>
-<stop offset="1" stop-color="#0B289B"/>
-</linearGradient>
-<linearGradient id="paint1_linear_2678_1058" x1="45.1879" y1="37.4131" x2="45.1879" y2="65.8924" gradientUnits="userSpaceOnUse">
-<stop stop-color="#15BAF6"/>
-<stop offset="1" stop-color="#2A50E3"/>
-</linearGradient>
-<linearGradient id="paint2_linear_2678_1058" x1="38.3301" y1="24.8818" x2="61.861" y2="66.7833" gradientUnits="userSpaceOnUse">
-<stop stop-color="#DFF1FF"/>
-<stop offset="1" stop-color="#A7CFEF"/>
-</linearGradient>
-</defs>
-</svg>
-
-
-
+          <svg width="110" height="110" viewBox="0 0 110 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_2678_1057)">
+              <circle cx="55.2032" cy="51.2967" r="49.075" fill="white"/>
+            </g>
+            <g filter="url(#filter1_f_2678_1057)">
+              <circle cx="58.5854" cy="37.4539" r="9.71759" fill="url(#paint0_linear_2678_1057)"/>
+            </g>
+            <g filter="url(#filter2_f_2678_1057)">
+              <path d="M66.4686 47.5625C70.2348 47.5625 73.2878 50.6157 73.288 54.3818V67.8633C73.2879 68.6164 72.6778 69.2274 71.9247 69.2275H45.246C44.4927 69.2275 43.8818 68.6165 43.8817 67.8633V54.3818C43.8819 50.6158 46.9351 47.5627 50.701 47.5625H51.8085C53.6035 48.8572 55.9796 49.6455 58.5858 49.6455C61.192 49.6455 63.5681 48.8571 65.3632 47.5625H66.4686Z" fill="url(#paint1_linear_2678_1057)"/>
+            </g>
+            <g opacity="0.84" filter="url(#filter3_f_2678_1057)">
+              <circle cx="51.8222" cy="39.2869" r="9.71759" fill="#DBECFA"/>
+              <path d="M59.7053 49.3955C63.4715 49.3956 66.5246 52.4487 66.5247 56.2148V69.6963C66.5245 70.4494 65.9136 71.0596 65.1604 71.0596H38.4827C37.7295 71.0596 37.1186 70.4494 37.1184 69.6963V56.2148C37.1185 52.4487 40.1716 49.3956 43.9377 49.3955H45.2463C47.0158 50.6052 49.3105 51.3379 51.8196 51.3379C54.3287 51.3379 56.6234 50.6052 58.3928 49.3955H59.7053Z" fill="#DBECFA"/>
+            </g>
+            <defs>
+              <filter id="filter0_d_2678_1057" x="0.501682" y="0.225183" width="109.403" height="109.403" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                <feMorphology radius="0.907499" operator="erode" in="SourceAlpha" result="effect1_dropShadow_2678_1057"/>
+                <feOffset dy="3.63"/>
+                <feGaussianBlur stdDeviation="3.267"/>
+                <feComposite in2="hardAlpha" operator="out"/>
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.07 0"/>
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2678_1057"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2678_1057" result="shape"/>
+              </filter>
+              <filter id="filter1_f_2678_1057" x="48.1177" y="26.9862" width="20.9355" height="20.9358" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="0.375074" result="effect1_foregroundBlur_2678_1057"/>
+              </filter>
+              <filter id="filter2_f_2678_1057" x="43.1316" y="46.8124" width="30.9065" height="23.1653" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="0.375074" result="effect1_foregroundBlur_2678_1057"/>
+              </filter>
+              <filter id="filter3_f_2678_1057" x="37.0502" y="29.5011" width="29.5426" height="41.6266" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="0.0340976" result="effect1_foregroundBlur_2678_1057"/>
+              </filter>
+              <linearGradient id="paint0_linear_2678_1057" x1="53.4458" y1="30.526" x2="66.9038" y2="47.1715" gradientUnits="userSpaceOnUse">
+                <stop offset="0.0817308" stopColor="#23A9F3"/>
+                <stop offset="0.4493" stopColor="#2775E7"/>
+                <stop offset="1" stopColor="#164181"/>
+              </linearGradient>
+              <linearGradient id="paint1_linear_2678_1057" x1="50.0966" y1="47.5625" x2="68.9265" y2="72.0988" gradientUnits="userSpaceOnUse">
+                <stop offset="0.0721154" stopColor="#24AAF4"/>
+                <stop offset="0.466346" stopColor="#2775E7"/>
+                <stop offset="1" stopColor="#164181"/>
+              </linearGradient>
+            </defs>
+          </svg>
 
           <h1 className={styles.title}>어민 생산자 정보 입력</h1>
         </div>
 
-    <div className={styles.whiteContainer}>
-        <div className={styles.section}>
-          <h2 className={styles.sectionTitle}>거래 위판장 지역</h2>
+        <div className={styles.whiteContainer}>
+          <div className={styles.section}>
+            <h2 className={styles.sectionTitle}>위판장 지역 선택</h2>
 
-          <div className={styles.provinceTabsContainer}>
-            {provinces.map((province) => (
-              <button
-                key={province}
-                onClick={() => setSelectedProvince(province)}
-                className={`${styles.provinceTab} ${selectedProvince === province ? styles.provinceTabActive : ""}`}
-              >
-                {province}
-              </button>
-            ))}
-          </div>
-        
-        {selectedProvince ? (
-          <div className={styles.locationGrid}>
-            {locations.map((location, index) => (
-              <div
-                key={index}
-                className={`${styles.locationCard} ${selectedLocation === location.name ? styles.locationCardSelected : ""}`}
-                onClick={() => setSelectedLocation(location.name)}
-              >
-                <div className={styles.locationCardContent}>
-                  <LocationIcon className={styles.locationIcon} />
-                  <div className={styles.locationInfo}>
-                    <div className={`${styles.locationName} ${selectedLocation === location.name ? styles.locationNameSelected : ""}`}>
-                      {location.name}
+            <div className={styles.provinceTabsContainer}>
+              {provinces.map((province) => (
+                <button
+                  key={province}
+                  onClick={() => handleProvinceSelect(province)}
+                  className={`${styles.provinceTab} ${selectedProvince === province ? styles.provinceTabActive : ""}`}
+                >
+                  {province}
+                </button>
+              ))}
+            </div>
+
+            {/* 도가 선택되었을 때만 위판장 목록 표시 */}
+            {selectedProvince && (
+              <div className={styles.locationGrid}>
+                {currentLocations.map((location, index) => (
+                  <div
+                    key={index}
+                    className={`${styles.locationCard} ${selectedLocation === location.name ? styles.locationCardSelected : ""}`}
+                    onClick={() => setSelectedLocation(location.name)}
+                  >
+                    <div className={styles.locationCardContent}>
+                      <LocationIcon className={styles.locationIcon} />
+                      <div className={styles.locationInfo}>
+                        <div className={`${styles.locationName} ${selectedLocation === location.name ? styles.locationNameSelected : ""}`}>
+                          {location.name}
+                        </div>
+                        <div className={styles.locationCity}>{location.city}</div>
+                      </div>
                     </div>
-                    <div className={styles.locationCity}>{location.city}</div>
                   </div>
-                </div>
+                ))}
               </div>
-            ))}
+            )}
+
+            {/* 도가 선택되지 않았을 때 안내 메시지 */}
+            {!selectedProvince && (
+              <div className={styles.emptyState}>
+                <p className={styles.emptyStateText}>위 지역 탭을 선택하여 위판장을 선택해주세요</p>
+              </div>
+            )}
           </div>
-          ) : (
-  <div className={styles.emptyState}>
-    <div className={styles.emptyMessage}>위의 지역을 선택하면 거래처가 표시됩니다</div>
-  </div>
-          )}
-        </div>
 
-        <div className={styles.inputSection}>
-          <label className={styles.inputLabel}>관리자명</label>
-          <input type="text" placeholder="예: 홍길동" className={styles.input} />
-        </div>
+          <div className={styles.inputSection}>
+            <label className={styles.inputLabel}>선박명/업체명</label>
+            <input type="text" placeholder="예: 대게잡이 1호" className={styles.input} />
+            <label className={styles.inputLabel}>선주명/대표자명</label>
+            <input type="text" placeholder="예: 홍길동" className={styles.input} />
+            <label className={styles.inputLabel}>전화번호</label>
+            <input type="text" placeholder="010-0000-0000" className={styles.input} />
+          </div>
 
-        <div className={styles.buttonContainer}>
-          <button className={styles.cancelButton}>취소</button>
-          <button className={styles.primaryButton}>✓ 회원가입 완료</button>
+          <div className={styles.buttonContainer}>
+            <button className={styles.cancelButton}>취소</button>
+            <button 
+              className={`${styles.primaryButton} ${!selectedLocation ? styles.primaryButtonDisabled : ""}`}
+              disabled={!selectedLocation}
+            >
+              ✓ 회원가입 완료
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   )
