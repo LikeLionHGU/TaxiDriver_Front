a07365b7e80eebeb822c72b2ac0ad5f4f24b1a64
Haeun <sunghaeun1004@gmail.com>
2025-08-23 22:07:02 +0900

    feat : 경매 상세페이지 거래 종료 버튼 구현

--- src/pages/AuctionDetail.js
+++ src/pages/AuctionDetail.js
@@ -5,6 +5,9 @@ import PageHeader from "../components/SalesSettlementPage/PageHeader";
 import AuctionItem from "../components/AuctionDetail/AuctionItem"
 import AuctionPeople from "../components/AuctionDetail/AuctionPeople"
 
+import stopbutton from "../assets/AuctionTable/stop.svg";
+import beforestopbutton from "../assets/AuctionTable/beforestop.svg";
+
 
 function AuctionDetail() {
   return (
@@ -12,6 +15,10 @@ function AuctionDetail() {
       <div className={styles.main}>
         <div className={styles.gradientBox}/>
         <div className={styles.pageContainer}>
+          <button className={styles.stopButton}>
+            <img src={stopbutton} alt="excel"/>
+            <span>거래 종료</span>
+          </button>
           <div className={styles.auctionLeft}>
             <PageHeader 
               image="note"
