fac7d50e8374197aaabd0f7ae8f86e830590315b
Haeun <sunghaeun1004@gmail.com>
2025-08-23 21:15:25 +0900

    feat : 경매 상세 왼쪽 컨테이너 디자인구현

---
+++ src/components/AuctionDetail/AuctionPeople.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./AuctionPeople.module.css";
+
+function AuctionPeople() {
+
+  return (
+    <>
+      <div className={styles.main}>
+        
+      </div>
+    </>
+  );
+}
+
+
+export default AuctionPeople;
--- src/pages/AuctionDetail.js
+++ src/pages/AuctionDetail.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "../styles/AuctionDetail.module.css";
 
 import PageHeader from "../components/SalesSettlementPage/PageHeader";
+import AuctionItem from "../components/AuctionDetail/AuctionItem"
+import AuctionPeople from "../components/AuctionDetail/AuctionPeople"
 
 
 function AuctionDetail() {
@@ -10,13 +12,14 @@ function AuctionDetail() {
       <div className={styles.main}>
         <div className={styles.gradientBox}/>
         <div className={styles.pageContainer}>
-          <div className={styles.auctionItem}>
+          <div className={styles.auctionLeft}>
             <PageHeader 
               image="note"
               title="경매 대기"
               content=""/>
+            <AuctionItem />
           </div>
-
+          <AuctionPeople />
         </div>
       </div>
     </>
