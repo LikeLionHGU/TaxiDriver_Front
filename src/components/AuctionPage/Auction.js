e9b886a5f93ec56b0d43b34902a68ce45e8281bf
Haeun <sunghaeun1004@gmail.com>
2025-08-24 16:00:27 +0900

    feat : 버튼, 검색 필터링 기능 구현

--- src/components/AuctionPage/Auction.js
+++ src/components/AuctionPage/Auction.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import styles from "./Auction.module.css";
 
@@ -8,6 +8,8 @@ import AuctionTable from "./AuctionTable";
 
 function Auction() {
   const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [activeStatus, setActiveStatus] = useState("ALL");
 
   return (
     <>
@@ -19,8 +21,10 @@ function Auction() {
             title="경매 상태"
             content="내가 등록한 상품의 경매 상태를 확인하세요."/>
           <div onClick={() => navigate(`/auction/detail`)}>상세 확인</div>
-          <SelcetButton />
-          <AuctionTable /> 
+          <SelcetButton value={activeStatus} onChange={setActiveStatus}/>
+          <AuctionTable activeStatus={activeStatus}
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}/> 
         </div>
       </div>
     </>
